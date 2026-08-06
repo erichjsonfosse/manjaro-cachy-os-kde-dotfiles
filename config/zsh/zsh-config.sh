defbcc1dc795290ff0aa197a3db5c9300a8101a2
Erich Json Fosse <erich@halloverden.no>
2026-08-06 04:30:36 +0200

    fix: no longer running ZSH immediately after installation

--- config/zsh/zsh-config.sh
+++ config/zsh/zsh-config.sh
@@ -8,7 +8,7 @@ export ZSH
 curl -fsSL -o install-ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod +x install-ohmyzsh.sh
 # Oh My Zsh install options
-su "$LOGNAME" -c "./install-ohmyzsh.sh --unattended"
+su "$LOGNAME" -c "RUNZSH=\"no\" ./install-ohmyzsh.sh --unattended"
 rm -f ./install-ohmyzsh.sh
 
 
