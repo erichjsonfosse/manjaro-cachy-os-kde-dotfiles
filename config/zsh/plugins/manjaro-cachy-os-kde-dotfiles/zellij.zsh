c9f2155aa954b1c96b531efead206148b8565aa7
Erich Json Fosse <erich@halloverden.no>
2026-08-08 14:24:25 +0200

    feat: defaulting to home folder

--- config/zsh/plugins/manjaro-cachy-os-kde-dotfiles/zellij.zsh
+++ config/zsh/plugins/manjaro-cachy-os-kde-dotfiles/zellij.zsh
@@ -2,6 +2,10 @@
 if command -v zellij &> /dev/null && [[ -z "$ZELLIJ" && -o interactive ]]; then
   # Check if the shell's parent process is yakuake or if we are inside a yakuake DBus context
   if [[ "$(ps -p $PPID -o comm= 2>/dev/null)" == *"yakuake"* || -n "$YAKUAKE_DBUS_WINDOW" ]]; then
+    # If starting in /, default to home folder so that Zellij sessions (and new tabs) start in $HOME
+    if [[ "$PWD" == "/" ]]; then
+      cd "$HOME" || exit
+    fi
     # Start zellij and attach to an existing session named 'yakuake' or create it
     exec zellij attach -c yakuake
   fi
