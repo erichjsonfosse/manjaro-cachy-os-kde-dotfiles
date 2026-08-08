1b50825893c390ace5a47a785d12e39d405a94af
Erich Json Fosse <erich@halloverden.no>
2026-08-08 18:03:26 +0200

    feat: lazy loading nvm hooks

--- config/zsh/plugins/nvm-init/nvm-hooks.plugin.zsh
+++ config/zsh/plugins/nvm-init/nvm-hooks.plugin.zsh
@@ -1,10 +1,32 @@
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-##### nvm init #####
-[ -z "%s" ] && export NVM_DIR="%s/.nvm" "$NVM_DIR" "$HOMEDIR"
-[[ -r "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"
-[[ -r "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
-##### nvm init #####
+# Establish NVM_DIR if not set
+if [ -z "$NVM_DIR" ]; then
+  export NVM_DIR="$HOME/.nvm"
+fi
 
+# High-Performance Lazy Loader for NVM and Node/NPM
+_load_nvm() {
+  # Unbind lazy functions to run only once
+  unset -f nvm node npm npx pnpm yarn
+  
+  # Source NVM configurations
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    \. "$NVM_DIR/nvm.sh"
+  fi
+  if [ -s "$NVM_DIR/bash_completion" ]; then
+    \. "$NVM_DIR/bash_completion"
+  fi
+}
+
+# Bind lazy triggers
+nvm() { _load_nvm; nvm "$@"; }
+node() { _load_nvm; node "$@"; }
+npm() { _load_nvm; npm "$@"; }
+npx() { _load_nvm; npx "$@"; }
+pnpm() { _load_nvm; pnpm "$@"; }
+yarn() { _load_nvm; yarn "$@"; }
+
+# Source helper hooks
 source "${0:A:h}"/angular-autocompletion.zsh
